f7e56420b1f96d8e8729f1ebd7ba22cb5ab369f5
Daniel Moya <danielmoya@MacBook-Pro-de-Daniel.local>
2023-10-23 18:22:07 -0400

    first commit

---
+++ Design2/Design2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Design2
+//
+//  Created by Daniel Moya on 23/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
